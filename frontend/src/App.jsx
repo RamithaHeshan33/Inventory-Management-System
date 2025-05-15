bbedb58dc28506b75204adac8f24f363fb4eab1e
Ramitha Heshan <ramithacampus@gmail.com>
2025-05-15 16:41:00 +0530

    components folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Login from './components/Login';
-import Register from './components/Register';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 function App() {
 
   return (
